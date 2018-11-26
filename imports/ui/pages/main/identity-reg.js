72bbb4cf1cc131972932dd5af0b04f0bd62ef34d
Amar <amarjss0094@gmail.com>
2018-11-26 17:44:21 +0530

    contract call

--- imports/ui/pages/main/identity-reg.js
+++ imports/ui/pages/main/identity-reg.js
@@ -3,6 +3,18 @@ import "../../stylesheets/identity-reg.css";
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import '../../../api/identity/methods';
+import  Eos from "eosjs";
+
+eosConfig = {
+    chainId: "e70aaab8997e1dfce58fbfac80cbbb8fecec7b99cf982a9444273cbc64c41473", // 32 byte (64 char) hex string
+    keyProvider: ['5KeNdWYxPbUpsLUa8QT64AbjTAQeHcZejcR6shHnNi1sESgxgm7'], // WIF string or array of keys..
+    httpEndpoint: 'https://jungle2.cryptolions.io:443',
+    expireInSeconds: 60,
+    broadcast: true,
+    verbose: false, // API activity
+    sign: true
+  }
+  eos = Eos(eosConfig)
 
 
 Template.identity_reg.onCreated(function () {
@@ -20,8 +32,13 @@ Template.identity_reg.events({
         var age = $('#age').val()
         var gender = $('#gender').val()
         var origin = $('#origin').val()
-        console.log(name)
+        console.log(name);
+        console.log(eos);
+        eos.contract('identityreg1').then(identityreg1 => {
+            identityreg1.addidentity("amartesttest","Amar",24,"male","INDIA",{authorization:"amartesttest"});
+          })
     },
 
 
 });
+
