b5f5cfc40199ce882c1232501766cedf3bf6446a
kstvigilante <kst85077@gmail.com>
2018-11-26 13:46:21 +0530

    added proposal page

--- imports/ui/pages/main/proposal.js
+++ imports/ui/pages/main/proposal.js
@@ -1,2 +1,11 @@
 import "./proposal.html"
 import "../../stylesheets/proposal.css";
+
+Template.App_proposal.events({
+    "click .new-proposal-button": function(){
+        console.log("create a new proposal");
+    },
+    "click .all-proposal-button": function(){
+        console.log("view all proposals");
+    }
+})
