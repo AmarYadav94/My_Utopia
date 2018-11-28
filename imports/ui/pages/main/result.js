a13e65790abe8326730b507469fc9411a9291a51
kstvigilante <kst85077@gmail.com>
2018-11-28 18:13:02 +0530

    changes to result

--- imports/ui/pages/main/result.js
+++ imports/ui/pages/main/result.js
@@ -3,6 +3,8 @@ import "../../stylesheets/result.css";
 import "./footer.js";
 import Eos from "eosjs";
 
+var result = [];
+
 eosConfig = {
     chainId: "e70aaab8997e1dfce58fbfac80cbbb8fecec7b99cf982a9444273cbc64c41473", // 32 byte (64 char) hex string
     keyProvider: ['5Jur4pK1Rb8xvdfNUUZJq5JE36HQUd9PNouWwjUdbWw7cK8ZuUo'],
@@ -24,7 +26,6 @@ Template.App_result.onRendered(async function(){
         json: true
     });
 
-    let result = [];
     var length = resultdata.rows[0].choices.length;
 
     for(var i=0; i<length;i++){
@@ -36,9 +37,6 @@ Template.App_result.onRendered(async function(){
             result[i][j] = 0;
         }
     }
-    
-    console.log("result array: ", result);
-    console.log("input :", resultdata);
 
     for(var i=0;i<resultdata.rows.length;i++){
         var choices = resultdata.rows[i].choices
@@ -50,4 +48,13 @@ Template.App_result.onRendered(async function(){
     }
 
     console.log("result array after calculating : ", result);
+    for(var i=0; i<result.length;i++){
+        console.log(" total votes for 1st position ", result[i][0]);
+        var total = result[i][0];
+        var length = 30*total;
+        var width = length+"px";
+        console.log("width ",width);
+        document.getElementsByClassName("candidate")[i].style.width = width;
+
+    }
 });
